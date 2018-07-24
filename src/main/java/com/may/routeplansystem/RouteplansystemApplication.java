2695a36d102e096eeeeb8f234663ce03dac2500f
Mayyj <may.yjie@gmail.com>
2018-07-24 14:17:54 +0800

    项目基础的修改

--- src/main/java/com/may/routeplansystem/RouteplansystemApplication.java
+++ src/main/java/com/may/routeplansystem/RouteplansystemApplication.java
@@ -1,8 +1,12 @@
 package com.may.routeplansystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.env.SpringApplicationJsonEnvironmentPostProcessor;
+import org.springframework.core.env.Environment;
 
+@MapperScan("com.may.routeplansystem.dao")
 @SpringBootApplication
 public class RouteplansystemApplication {
 
