9311847122448258639bc87eb5ed65f4aaa1e587
Genius_DSY <genius_dsy@foxmail.com>
2018-11-18 11:08:16 +0800

    update

--- src/main/java/com/may/routeplansystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/may/routeplansystem/service/impl/UserServiceImpl.java
@@ -19,6 +19,9 @@ import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * @author:dengsiyuan
@@ -29,8 +32,9 @@ import java.util.Date;
 @Import(value = MailSenderAutoConfiguration.class)
 public class UserServiceImpl implements UserService {
 
+    String attribute = "user";
+    String codeAttribute = "verifyCode";
     private String from = "campus.mis@foxmail.com";
-
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
@@ -41,77 +45,167 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 验证登陆信息是否正确
-     * @param userMessage
+     * @param userId
+     * @param password
+     * @param code
+     * @param session
      * @return true:验证通过 false:验证不通过
      * */
     @Override
-    public int userLogin(UserMessage userMessage, HttpSession session) {
+    public Object userLogin(String userId, String password, String code, HttpSession session){
+        String sessionCode = (String) session.getAttribute(codeAttribute);
+        Map map = new HashMap<String,Object>(16);
+        UserMessage userMessage = new UserMessage();
         try {
-            if(userDao.isLogin(userMessage) != null){
-                session.setAttribute("user",userMessage.getUserId());
-                session.setMaxInactiveInterval(3600);
-                return StatusCode.SUCCESS;
+            if(session.getAttribute(attribute) != null){
+                map.put("status", StatusCode.SUCCESS);
             }else {
-                return StatusCode.MESSAGE_ERROR;
+                if (userId != null && password !=null && code != null){
+                    if (code.equalsIgnoreCase(sessionCode)){
+                        userMessage.setUserId(userId);
+                        userMessage.setPassword(password);
+                        if(userDao.isLogin(userMessage) != null){
+                            session.setAttribute("user",userMessage.getUserId());
+                            session.setMaxInactiveInterval(3600);
+                            map.put("status",StatusCode.SUCCESS);
+                            map.put("userMessage",userDao.userMessage(userId));
+                        }else {
+                            map.put("status",StatusCode.MESSAGE_ERROR);
+                        }
+                    }else {
+                        map.put("status",StatusCode.CODE_FAIL);
+                    }
+                }else {
+                    map.put("status",StatusCode.MESSAGE_NULL);
+                }
             }
         }catch (Exception e){
             logger.error(e.getClass()+"{}",e);
-            return StatusCode.FAIL;
+            map.put("status", StatusCode.FAIL);
         }
+        return map;
     }
 
     /**
      * 用户注册
-     *
      * @param userMessage
+     * @param mailCode
+     * @param rePassword
+     * @param session
      * @return -1:注册失败
      */
     @Override
-    public int userRegister(UserMessage userMessage) {
-        try{
-            if(userDao.insertUser(userMessage) == -1){
-                return StatusCode.PERMISSION_FAIL;
+    public Object userRegister(UserMessage userMessage,String mailCode,String rePassword,HttpSession session) {
+        Map map = new HashMap<String,String>(16);
+        String verifyCode = (String) session.getAttribute("mailCode");
+        try {
+            if(userMessage == null){
+                map.put("status",StatusCode.MESSAGE_NULL);
             }else {
-                return StatusCode.SUCCESS;
+                if(userDao.userMessage(userMessage.getUserId()) != null){
+                    map.put("status",StatusCode.MESSAGE_EXIST);
+                }else {
+                    if (rePassword.equals(userMessage.getPassword())){
+                        if(mailCode.equals(verifyCode)){
+                            if(userDao.insertUser(userMessage) == -1){
+                                map.put("status",StatusCode.PERMISSION_FAIL);
+                            }else {
+                                map.put("status",StatusCode.SUCCESS);
+                            }
+                        }else {
+                            map.put("status",StatusCode.CODE_FAIL);
+                        }
+                    }else {
+                        map.put("status",StatusCode.MESSAGE_ERROR);
+                    }
+                }
             }
         }catch (Exception e){
+            map.put("status",StatusCode.FAIL);
             logger.error(e.getClass()+"{}",e);
-            return StatusCode.FAIL;
         }
+        return map;
     }
 
     /**
      * 用户邮箱验证码发送
-     * @param to
-     * @param subject
-     * @param content
+     * @param eMail
+     * @param session
      * @return 邮件
      * */
     @Override
-    public Object sendVerifyMail(String to,String subject,String content) {
-        MimeMessage message = null;
+    public Object sendVerifyMail(String eMail,HttpSession session) {
+        Map map = new HashMap<String,Object>(16);
         try {
-            message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom(from);
-            helper.setTo(to);
-            helper.setSubject(subject);
-            Date date = new Date();
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            StringBuffer sb = new StringBuffer();
-            sb.append("<div style='position: relative; margin: 0 auto; width: 450px;height: 350px;'>")
-                    .append("<h1 align='center'><strong>Path</strong></h1>")
-                    .append("<h5>您好：</h5>")
-                    .append("<h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;欢迎您通过邮箱验证码来验证您的身份！</h5>")
-                    .append("<h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;快！使用<div style='position: relative; margin: 0 auto; width: 100px;height: 45px; text-align: center; background-color: #EEE'><font size='5' style='position: relative; top: 6px;'>" + content + "</font></div>去验证您的身份吧！切记不要告诉他人哦！</h5>")
-                    .append("<h5 style='position:relative; float: right; margin-bottom: 0'>Path</h5><br><br>")
-                    .append("<h5 style='position: relative; float: right; margin-top: 0'>" + simpleDateFormat.format(date) + "</h5></div>");
-            helper.setText(sb.toString(), true);
-            mailSender.send(message);
-            return null;
+            if(userDao.userEmail(eMail) == null){
+                //生成验证码
+                String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+                Random random = new Random();
+                StringBuffer code = new StringBuffer();
+                for (int i = 0; i < 5; i++) {
+                    int number = random.nextInt(base.length());
+                    code.append(base.charAt(number));
+                }
+                //存入Session
+                session.setAttribute("mailCode",code.toString());
+                MimeMessage message = null;
+                //创建一个邮件
+                message = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(message, true);
+                //设置邮件头部
+                helper.setFrom(from);
+                helper.setTo(eMail);
+                helper.setSubject("Path Security");
+                Date date = new Date();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                //拼接邮件内容
+                StringBuffer sb = new StringBuffer();
+                sb.append("<div style='position: relative; margin: 0 auto; width: 450px;height: 350px;'>")
+                        .append("<h1 align='center'><strong>Path</strong></h1>")
+                        .append("<h5>您好：</h5>")
+                        .append("<h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;欢迎您通过邮箱验证码来验证您的身份！</h5>")
+                        .append("<h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;快！使用<div style='position: relative; " +
+                                "margin: 0 auto; width: 100px;height: 45px; text-align: center; background-color: #EEE'>" +
+                                "<font size='5' style='position: relative; top: 6px;'>"
+                                + code.toString()
+                                + "</font></div>去验证您的身份吧！切记不要告诉他人哦！</h5>")
+                        .append("<h5 style='position:relative; float: right; margin-bottom: 0'>Path</h5><br><br>")
+                        .append("<h5 style='position: relative; float: right; margin-top: 0'>" + simpleDateFormat.format(date) + "</h5></div>");
+                helper.setText(sb.toString(), true);
+                //发送邮件
+                mailSender.send(message);
+                map.clear();
+                map.put("status",StatusCode.SUCCESS);
+            }else {
+                map.clear();
+                map.put("status",StatusCode.MESSAGE_ERROR);
+            }
         } catch (MessagingException e) {
             logger.error(e.getClass() + "{}", e);
-            return null;
+            map.clear();
+            map.put("status",StatusCode.FAIL);
         }
+        return map;
+    }
+
+    /**
+     * 注册userId唯一性验证
+     *
+     * @param userId
+     * @return userMessage
+     */
+    @Override
+    public Object userIdVerify(String userId) {
+        Map map = new HashMap<String,Object>(16);
+        try {
+            if(userDao.userMessage(userId) != null){
+                map.put("status",StatusCode.MESSAGE_ERROR);
+            }else {
+                map.put("status",StatusCode.SUCCESS);
+            }
+        }catch (Exception e){
+            map.put("status",StatusCode.FAIL);
+        }
+        return map;
     }
 }
